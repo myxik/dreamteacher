50aaa6776ec5f6769482d6fe77b1a261ea4edc98
myxik <myxikpupsik@gmail.com>
2023-10-22 12:40:10 +0000

    added logger, minor fixes

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import torch as T
-import torchvision
+import lovely_tensors as lj
 import torchvision.transforms as tr
 
 from datasets import load_dataset
 from tqdm.auto import tqdm
 from argparse import ArgumentParser
+from torch.utils.tensorboard.writer import SummaryWriter
 
 from dataset import HFDataset
 from backbone import ImageBackbone
@@ -12,12 +13,15 @@ from unet import Generator
 from loss import DTLoss
 
 
+lj.monkey_patch()
+
+
 def parse_args():
     p = ArgumentParser()
     p.add_argument("--lr", default=1e-3, type=float, required=False)
     p.add_argument("--num_epochs", default=300, type=int, required=False)
     p.add_argument("--device", default="cuda:1", type=str, required=False)
-    p.add_argument("--batch_size", default=8, type=int, required=False)
+    p.add_argument("--batch_size", default=16, type=int, required=False)
     p.add_argument("--gamma", default=10, type=float, required=False)
     p.add_argument("--image_size", default=256, type=int, required=False)
     return p.parse_args()
@@ -41,7 +45,7 @@ if __name__ == "__main__":
     dataset = load_dataset("./imagenet")
 
     imagenet_train = HFDataset(dataset["train"], train_transforms)
-    imagenet_valid = HFDataset(dataset["valid"], valid_transforms)
+    imagenet_valid = HFDataset(dataset["validation"], valid_transforms)
 
     trainloader = T.utils.data.DataLoader(
         imagenet_train, batch_size=args.batch_size, shuffle=True, pin_memory=True
@@ -61,6 +65,10 @@ if __name__ == "__main__":
 
     optimizer = T.optim.SGD(f.parameters(), lr=args.lr, nesterov=True, momentum=0.99)
 
+    run_name = "DT-Exp1"
+    writer = SummaryWriter(f"./logs/{run_name}")
+
+    global_iters = 0
     for e in range(args.num_epochs):
         for images, _ in tqdm(trainloader):
             images = images.to(device)
@@ -73,3 +81,6 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            writer.add_scalar("monitor/loss", loss.item(), global_iters)
+            global_iters += 1
